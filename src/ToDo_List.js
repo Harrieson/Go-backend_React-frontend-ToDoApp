5c5344a1b612d6bd16f8ae93988fb495a762a7b6
Harrieson <harriemburu@gmail.com>
2022-03-26 01:44:25 +0300

    Models and routes

--- src/ToDo_List.js
+++ src/ToDo_List.js
@@ -18,5 +18,29 @@ class ToDo_list extends Component {
     ComponentDidMount(){
     this.GetTask();
     };
+
+    render() {
+        return (
+            <div>
+                <div className='row'>
+                    <Header className='header' as='h2' color='blue'>
+                        ToDo_list
+                    </Header>
+                </div>
+                <div className='row'>
+                    <Form onSubmit={this.onSubmit}>
+                        <Input
+                            type='text'
+                            name='task'
+                            onChange={this.onChange}
+                            value={this.state.task}
+                            fluid
+                            placeholder='Create Task' />
+                    </Form>
+                </div>
+            </div>
+        )
+    }
 }
 
+export default ToDo_list
